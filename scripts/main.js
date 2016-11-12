4b2fdbbb33c6f8d79bb92e46b4723c158c1746f9
Michelle Chen <chen.michelle15@gmail.com>
2016-11-12 15:41:08 -0500

    trying new thing

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,23 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World';
+
+alert("hello!");
+
+d3.select("body")
+  .append("svg")
+  .attr("width", 50)
+  .attr("height", 50)
+  .append("circle")
+  .attr("cx", 25)
+  .attr("cy", 25)
+  .attr("r", 25)
+  .style("fill", "purple");
+
+var theData = [ 1, 2, 3 ]
+
+var p = d3.select("body").selectAll("p")
+		  .data(theData)
+		  .enter()
+		  .append("p")
+		  .text("hello ");
+
